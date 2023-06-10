cce16fe3a87e4692925b7ae2ad0e8b71e93e182f
dingming <ddd>
2023-06-10 22:16:44 +0800

    save

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import {BrowserRouter,Routes,Route} from 'react-router-dom'
-import Login from './pages/Login';
-import Layout from './pages/Layout';
+import Login from '@/pages/Login';
+import Layout from '@/pages/Layout';
 
 function App() {
   return (
