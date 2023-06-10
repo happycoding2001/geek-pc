9be56dd65b872b16a2618b955424a2600bdfdb3d
dingming <ddd>
2023-06-10 20:44:12 +0800

    P102 pc项目-配置一级基础路由 07:20

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import './App.css';
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import Login from './pages/Login';
+import Layout from './pages/Layout';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-       
-          Learn React
-       
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+            <Routes>
+              <Route path='/login' element={<Login />}></Route>
+              <Route path='/layout' element={<Layout />}></Route>
+            </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
