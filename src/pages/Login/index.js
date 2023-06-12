112d8bb97a73c0d3b18be02e68e45414369c6efd
dingming <ddd>
2023-06-12 19:05:12 +0800

    P107 pc项目-登录-搭建静态结构

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,7 +1,16 @@
-function Login(){
-    return <div>
-        login
+import { Card } from 'antd'
+import logo from '@/assets/logo.png'
+import './index.scss'
+
+const Login = () => {
+  return (
+    <div className="login">
+      <Card className="login-container">
+        <img className="login-logo" src={logo} alt="" />
+        {/* 登录表单 */}
+      </Card>
     </div>
+  )
 }
 
 export default Login
